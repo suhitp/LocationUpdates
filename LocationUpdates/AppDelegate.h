ce0b23d0226b3bb9f4c6f2e9a4b91ec5a4717e55
suhit.patil <suhit.patil@globallogic.com>
2014-12-22 23:59:21 +0530

    Initial Commit

---
+++ LocationUpdates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationUpdates
+//
+//  Created by Suhit on 22/12/14.
+//  Copyright (c) 2014 com.suhit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationUpdates/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocationUpdates
+//
+//  Created by Suhit on 22/12/14.
+//  Copyright (c) 2014 com.suhit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
